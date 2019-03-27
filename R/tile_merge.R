c5334430b412b942ca7aaf998c46fd938e99d536
kapitzas <simon.statecology@gmail.com>
2019-03-27 17:03:21 +1100

    Aggregation of tiles before merging is possible

--- R/tile_merge.R
+++ R/tile_merge.R
@@ -6,10 +6,11 @@
 #' @import sp
 #' @import stringr
 
-#' @param tiles "List with 0.5 arc min WorldClim tile stacks (or single rasters) (i.e. produced by \code{tile_get}). Both stacks have to have the same variables in them.
+#' @param tiles List with 0.5 arc min WorldClim tile stacks (or single rasters) (i.e. produced by \code{tile_get}). Names and number of variables need to match between stacks.
+#' @param fac Factor by which resolution is changed, see details.
 #' @return RasterStack or Raster containing merged WorldClim variable(s).
 #'
-#' @details \code{raster::merge} is used for merging, at default settings. That means that in case of overlaps the cell values of the object that comes first in the sequence are retained. But this shouldn't matter much, because there are no overlaps between WorldClim tiles.
+#' @details \code{raster::merge} is used for merging, at default settings. That means that in case of overlaps the cell values of the object that comes first in the sequence are retained. Raster resolution can be changed to speed up merging of very high resolution data. For example, when data are SRTM tiles, \code{fac = 10} will resample to a resolution of 0.5 arcmin (bilinear), which corresponds to the resolution of WorldClim data.
 #'
 #' @author Simon Kapitza \email{simon.statecology@gmail.com}
 #' @export
@@ -25,8 +26,9 @@
 #'
 #' wcmerged <- tile_merge(wctiles)
 #' srtmmerged <- tile_merge(srtmtiles)
+#' srtmmerged <- tile_merge(srtmtiles, fac = 10)
 
-tile_merge <- function(tiles){
+tile_merge <- function(tiles, fac){
 
   if(!is.list(tiles)) stop("Please provide list with stacks for each tile")
 
@@ -41,14 +43,31 @@ tile_merge <- function(tiles){
     if(!all(sapply(tiles, nlayers) == nlayers(tiles[[1]]))) stop("Supplied tiles have different number of variables")
     if(!length(unique(sapply(sapply(tiles, names), FUN = function(x) strsplit(x, '_')[[1]][1])))   == nlayers(tiles[[1]])) stop("Variable names differ between tiles")
     out <- list()
-    message("Merging tiles...")
+    crs <- crs(tiles[[1]])
+    res <- res(tiles[[1]])
+
+    if(!missing(fac)){
+      res <- res * fac
+    }
+
     for (i in 1:nlayers(tiles[[1]])){
-      b <- tiles[[1]][[i]]
-      message("Layer", i, "/", length(layernames))
+
+      if(!missing(fac)){
+        b <- projectRaster(tiles[[1]][[i]], res = res, crs = crs)
+      }else{
+        b <- tiles[[1]][[i]]
+      }
+
+      message("Layer ", i, "/", length(layernames))
 
       for(j in 2:length(tiles)){
-        message("Tile ", j)
-        b <- raster::merge(b, tiles[[j]][[i]])
+        message("Adding tile ", j)
+        if(!missing(fac)){
+          b2 <- projectRaster(tiles[[j]][[i]], res = res, crs = crs)
+        }else{
+          b2 <- tiles[[j]][[i]]
+        }
+        b <- raster::merge(b, b2)
       }
 
       out[[i]] <- b
